a33272ad9ce77f617f8fa5314f7f4d3d487855f1
tqchen <tqchen@cs.washington.edu>
2014-02-06 10:40:57 -0800

    initial commit

---
+++ demo/mnist-sghmc.py
@@ -0,0 +1,23 @@
+# demo for mnist experiment
+import sys
+sys.path.append('..')
+import mnist
+
+# get default prameters
+param = mnist.cfg_param()
+
+# set necessary parameters we want
+param.batch_size = 500
+param.num_round = 800
+param.num_hidden = 100
+param.path_data = '../../../../data/mnist'
+param.net_type = 'mlp2'
+param.updater  = 'hmc'
+param.hyperupdater = 'gibbs-sep'
+param.num_burn     = 50
+
+param.eta=0.1
+param.mdecay=0.01
+
+# run the experiment
+mnist.run_exp( param )
