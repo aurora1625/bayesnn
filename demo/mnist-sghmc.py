ba6bc9c3709fcce353b70dc596f9d493648fa780
jasonxu90 <jasonxu@uw.edu>
2014-02-16 15:30:17 -0800

    Merck changes
    Wrote loading file, changed parameter initiation and demo file. Began
    changes in nnet evaluator, currently not returning proper regression
    predictions yet

--- demo/mnist-sghmc.py
+++ demo/mnist-sghmc.py
@@ -12,12 +12,12 @@ param.num_round = 800
 param.num_hidden = 100
 
 # change the following line to PATH/TO/MNIST dataset
-param.path_data = '../../../../data/mnist'
+param.path_data = '/Users/jasonxu/bayesnn/'
 
 param.net_type = 'mlp2'
 param.updater  = 'hmc'
 param.hyperupdater = 'gibbs-sep'
-param.num_burn     = 50
+param.num_burn = 50
 
 param.eta=0.1
 param.mdecay=0.01
