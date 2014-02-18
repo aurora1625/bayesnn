5117964392d566138dda13400bb2d44bcc456f69
tqchen <tqchen@cs.washington.edu>
2014-02-18 09:45:17 -0800

    comments for mnist

--- demo/mnist-sghmc.py
+++ demo/mnist-sghmc.py
@@ -7,19 +7,28 @@ import mnist
 param = mnist.cfg_param()
 
 # set necessary parameters we want
+
 param.batch_size = 500
+# number of total rounds run
 param.num_round = 800
+
 param.num_hidden = 100
 
 # change the following line to PATH/TO/MNIST dataset
 param.path_data = '../../../../data/mnist'
 
+# network type
 param.net_type = 'mlp2'
+# updating method
 param.updater  = 'hmc'
+# hyper parameter sampling: use gibbs for each parameter group
 param.hyperupdater = 'gibbs-sep'
+# number of burn-in round, start averaging after num_burn round
 param.num_burn = 50
 
+# learning rate
 param.eta=0.1
+# alpha, momentum decay
 param.mdecay=0.01
 
 # run the experiment
